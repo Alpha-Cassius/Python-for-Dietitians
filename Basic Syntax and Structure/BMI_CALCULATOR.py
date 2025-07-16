87c6e98372af8fbf07c2a6ee193e0a56e6f1a76f
Vaibhav Pandey <vaibhavpandey2610@gmail.com>
2025-07-16 23:30:58 +0530

    Create BMI_CALCULATOR.py

---
+++ Basic Syntax and Structure/BMI_CALCULATOR.py
@@ -0,0 +1,6 @@
+# Basic Python syntax example with dietary calculation
+def calculate_bmi(weight, height):
+    bmi = weight / (height ** 2)
+    return bmi
+if main == 'name':
+  calculate_bmi(40, 150)
